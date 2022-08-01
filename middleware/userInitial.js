08f88d1118d6338731e71cfba3c54c0755220622
gamadjg <gamadavid36@gmail.com>
2022-08-01 08:43:47 -0700

    Converted user icon from picture to displayname initials

---
+++ middleware/userInitial.js
@@ -0,0 +1,7 @@
+function getUserInitial(user) {
+	console.log(user.userDisplayName);
+	//const userInitial = user.userDisplayName.characterAt(0);
+	//return userInitial;
+}
+
+module.exports = { getUserInitial };
