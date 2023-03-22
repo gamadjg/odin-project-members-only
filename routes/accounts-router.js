7842beb275a45d022f322cc597f7a5deceb62d0b
gamadjg <gamadavid36@gmail.com>
2023-03-21 17:21:04 -0700

    Added 'async' onto Express-validator's check method within the sign-up router

--- routes/accounts-router.js
+++ routes/accounts-router.js
@@ -5,96 +5,95 @@ const accountController = require("../controllers/account-controller");
 const passport = require("passport");
 
 router.get("/", (req, res) => {
-  res.render("index", {
-    title: "User Account",
-    page: "account-user",
-    user: req.user,
-  });
+	res.render("index", {
+		title: "User Account",
+		page: "account-user",
+		user: req.user,
+	});
 });
 
 router.get("/log-in", function (req, res) {
-  res.render("index", {
-    title: "Account Login",
-    page: "account-log-in",
-    user: req.user,
-  });
+	res.render("index", {
+		title: "Account Login",
+		page: "account-log-in",
+		user: req.user,
+	});
 });
 
 router.post("/log-in", (req, res, next) => {
-  passport.authenticate("local", (err, user, info, status) => {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      return res.render("index", {
-        title: "Account Login",
-        page: "account-log-in",
-        user: req.user,
-        alert: [info.message],
-      });
-    }
-    req.login(user, function (err) {
-      if (err) {
-        return next(err);
-      }
-      return res.redirect("/");
-    });
-  })(req, res, next);
+	passport.authenticate("local", (err, user, info, status) => {
+		if (err) {
+			return next(err);
+		}
+		if (!user) {
+			return res.render("index", {
+				title: "Account Login",
+				page: "account-log-in",
+				user: req.user,
+				alert: [info.message],
+			});
+		}
+		req.login(user, function (err) {
+			if (err) {
+				return next(err);
+			}
+			return res.redirect("/");
+		});
+	})(req, res, next);
 });
 
 router.get("/sign-up", (req, res) => {
-  res.render("index", {
-    title: "Account Sign Up",
-    page: "account-sign-up",
-    user: req.user,
-  });
+	res.render("index", {
+		title: "Account Sign Up",
+		page: "account-sign-up",
+		user: req.user,
+	});
 });
 
 router.post(
-  "/sign-up",
-  [
-    check("username", "Email is not valid.").isEmail(),
-    check("password", "Missing password.").exists(),
-    check("confirmPassword", "Passwords do not match.")
-      .trim()
-      .custom((confirmPassword, { req }) => {
-        const password = req.body.password;
-
-        if (password !== confirmPassword) {
-          throw new Error("Passwords must be same");
-        }
-      }),
-  ],
-  (req, res) => {
-    const errors = validationResult(req);
-    if (errors) {
-      const alert = errors.array();
-      res.render("index", {
-        title: "Account Sign Up",
-        page: "account-sign-up",
-        user: req.user,
-        alert,
-      });
-    } else {
-      accountController.createUser(req, res);
-    }
-  }
+	"/sign-up",
+	[
+		check("username", "Email is not valid.").isEmail(),
+		check("password", "Missing password.").exists(),
+		check("confirmPassword", "Passwords do not match.")
+			.trim()
+			.custom(async (confirmPassword, { req }) => {
+				const password = req.body.password;
+				if (password !== confirmPassword) {
+					throw new Error("Passwords must be same");
+				}
+			}),
+	],
+	(req, res) => {
+		const errors = validationResult(req);
+		if (errors.array().length != 0) {
+			const alert = errors.array();
+			res.render("index", {
+				title: "Account Sign Up",
+				page: "account-sign-up",
+				user: req.user,
+				alert,
+			});
+		} else {
+			accountController.createUser(req, res);
+		}
+	}
 );
 
 router.get("/update-account-type", (req, res) => {
-  res.render("index", {
-    title: "Update Membership Form",
-    page: "account-membership-update",
-    user: req.user,
-  });
+	res.render("index", {
+		title: "Update Membership Form",
+		page: "account-membership-update",
+		user: req.user,
+	});
 });
 
 router.post("/update-account-type", (req, res) => {
-  accountController.updateAccountType(req, res);
+	accountController.updateAccountType(req, res);
 });
 
 router.get("/log-out", (req, res) => {
-  accountController.logout(req, res);
+	accountController.logout(req, res);
 });
 
 module.exports = router;
